94ceceddbc71469d25b7d4b854426ac5b793f8fb
qs100 <160395032+qs100@users.noreply.github.com>
2024-08-28 10:25:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,3 @@ PF_number = "PF00067"
 evaluation_threshold = "1e-5"
 
 species = "sesame"
-
-# The repository only contains the genome protein sequences of three species: Arabidopsis_thaliana , Oryza_sativa , Lolium_perenne , and Zea_mays
-git add .
-git commit -m "update"
-git push
