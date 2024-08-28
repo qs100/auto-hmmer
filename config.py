951d785276fa2f018d2d2dc199f69469c9d0416c
qs100 <160395032+qs100@users.noreply.github.com>
2024-08-28 10:57:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-PF_number = "PF02298"
+PF_number = "PF00067"
 
 evaluation_threshold = "1e-5"
 
-species = "Arabidopsis_thaliana"
+species = "sesame"
 
 # The repository only contains the genome protein sequences of three species: Arabidopsis_thaliana , Oryza_sativa , Lolium_perenne , and Zea_mays
